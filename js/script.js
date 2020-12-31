79c6e130d01024cf291d47dcc6a1287ddd318eae
Muhammad Zulfa <62686312+muhammadzulfa@users.noreply.github.com>
2020-12-31 18:25:49 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,21 +6,76 @@ $(window).on("scroll", function () {
    }
 });
 
-$("#go-fb").click(function () {
-   window.location.href = "https://www.facebook.com/muhammadzulfacom";
-})
+$('.fireworks').fireworks({
+   sound: true,
+   opacity: 0.8,
+   width: '100%',
+   height: '100%'
+});
 
-$("#go-tl").click(function () {
-   window.location.href = "https://t.me/mhmmdzlfa";
-})
+// Set date
+var countdownDate = new Date(
+   "January 1, 2021 00:00:00"
+).getTime(); /* hrs: min: sec */
 
-$("#go-ig").click(function () {
-   window.location.href = "https://www.instagram.com/mhmmd.zlfa";
-})
+// Update the count down every 1 second
+var x = setInterval(function () {
+   // Get todays date and time
+   var now = new Date().getTime();
 
-$("#go-github").click(function () {
-   window.location.href = "https://www.github.com/muhammadzulfa";
-})
+   // Find the distance between now and the count down date
+   var distance = countdownDate - now;
+
+   // Time calculations for days, hours, minutes and seconds
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+   // Display the result in the element
+   document.querySelector("#d").innerText = addZero(days);
+   document.querySelector("#h").innerText = addZero(hours);
+   document.querySelector("#m").innerText = addZero(minutes);
+   document.querySelector("#s").innerText = addZero(seconds);
+
+   // If the count down is finished, write some text
+   if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown-box").innerHTML =
+         "<span class='cdi'>Happy New Year!!</span>";
+      document.querySelector(".title").innerText = "";
+   }
+}, 1000);
+
+function addZero(i) {
+   if (i < 10) {
+      i = "0" + i;
+   }
+   return i;
+}
+
+function isMobile() {
+   if (window.innerWidth < 775) {
+      return true;
+   } else {
+      return false;
+   }
+}
+
+/********************
+    Animation Frame
+  ********************/
+
+window.requestAnimFrame = (function () {
+   return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      function (callback) {
+         window.setTimeout(callback, 1000 / 60);
+      }
+   );
+})();
 
 particlesJS("particles-js", {
    "particles": {
@@ -153,6 +208,22 @@ update = function () {
    requestAnimationFrame(update);
 };
 requestAnimationFrame(update);
+
+$("#go-fb").click(function () {
+   window.location.href = "https://www.facebook.com/muhammadzulfacom";
+})
+
+$("#go-tl").click(function () {
+   window.location.href = "https://t.me/mhmmdzlfa";
+})
+
+$("#go-ig").click(function () {
+   window.location.href = "https://www.instagram.com/mhmmd.zlfa";
+})
+
+$("#go-github").click(function () {
+   window.location.href = "https://www.github.com/muhammadzulfa";
+})
 
 $(window).on("scroll", function () {
    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
